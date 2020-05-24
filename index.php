36f7781111ea0c2dc99fa47a4ecff4ed18fa7332
Farhan Naufal <vnalvaro24@yahoo.com>
2020-05-24 09:10:47 +0700

    Update index.php

--- index.php
+++ index.php
@@ -4,18 +4,18 @@ $server_key = "SB-Mid-server-JyBIr1T-w4HUwWBdqDEhpXDn";
 $is_production = false;
 $api_url = $is_production ? 'https://app.midtrans.com/snap/v1/transactions' : 'https://app.sandbox.midtrans.com/snap/v1/transactions';
 
-if(!strois($_SERVER['REQUEST_URI'], '/charge')){
+if(!strpos($_SERVER['REQUEST_URI'], '/charge')){
     http_response_code(404);
-    echo "wrong path, make sure it;s '/charge'"; exit();
+    echo "wrong path, make sure it's '/charge'"; exit();
 }
 
 if($_SERVER['REQUEST_METHOD'] !== 'POST'){
  http_response_code(404);
- echo " Halaman Tidak Ditemukan atau Permintaan HTTP Telah Digunakan"; exit();
+ echo "Page not found or wrong HTTP request method is used"; exit();
 }
 
 $request_body = file_get_contents('php://input');
-header('Content-Type: Application/json');
+header('Content-Type: application/json');
 
 $charge_result = chargeAPI($api_url, $server_key, $request_body);
 
